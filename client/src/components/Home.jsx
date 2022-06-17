7f6f780f06a4429c053b6945281d7b09bf7f0a85
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2022-06-18 00:50:37 +0500

    feat: changed routing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const Home = () => {
-    return <h1>Home</h1>
-}
+  return (
+    <>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import Home from './components/Home.jsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   </BrowserRouter>
 );
