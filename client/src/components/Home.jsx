92793cc0e6467f44a12db7729686cca33c4baec5
useracct <basilyusuf1709@gmail.com>
2022-06-17 17:08:14 -0400

    Navbar and routing

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Navbar from './Navbar/Navbar';
 
 const Home = () => {
   return (
-    <>
+    <div style={{
+      position: 'absolute',
+      left: '100px',
+    }}>
       <div className="card" style={{ width: '18rem' }}>
         <div className="card-body">
           <h5 className="card-title">zainsajid</h5>
@@ -15,7 +19,68 @@ const Home = () => {
           </a>
         </div>
       </div>
-    </>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+      <div className="card" style={{ width: '18rem' }}>
+        <div className="card-body">
+          <h5 className="card-title">zainsajid</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a href="#" className="btn btn-primary">
+            Comments
+          </a>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './components/Home.jsx';
+import Navbar from './components/Navbar/Navbar.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,5 +11,6 @@ root.render(
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
+    <Navbar />
   </BrowserRouter>
 );
