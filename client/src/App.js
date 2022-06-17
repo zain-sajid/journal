0bc58d3c50b60674c2a14a588b34b4eda1b2ab85
useracct <basilyusuf1709@gmail.com>
2022-06-17 15:08:20 -0400

    Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import RouteHandler from './routes/RouteHandler'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <RouteHandler />
     </div>
   );
 }
