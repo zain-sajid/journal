2008ca4de110ea5596175e4e33a5aa47d5483399
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2022-06-18 02:08:55 +0500

    feat: changes to homepage

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,20 +2,59 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <>
-      <div className="card" style={{ width: '18rem' }}>
-        <div className="card-body">
-          <h5 className="card-title">zainsajid</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Comments
-          </a>
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <div className="card mt-4" style={{ width: '80%' }}>
+          <div className="card-body">
+            <h5 className="card-title">zainsajid</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Some quick example text to build on
+              the card title and make up the bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary me-2">
+              Discuss
+            </a>
+            <button className="btn btn-primary" type="submit">
+              <i class="bi bi-hand-thumbs-up"></i>
+            </button>
+          </div>
+        </div>
+        <div className="card mt-4" style={{ width: '80%' }}>
+          <div className="card-body">
+            <h5 className="card-title">zainsajid</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Some quick example text to build on
+              the card title and make up the bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary me-2">
+              Discuss
+            </a>
+            <button className="btn btn-primary me-2" type="submit">
+              <i class="bi bi-hand-thumbs-up"></i>
+            </button>
+            <span className="bold">+2</span>
+          </div>
+        </div>
+        <div className="card mt-4" style={{ width: '80%' }}>
+          <div className="card-body">
+            <h5 className="card-title">zainsajid</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Some quick example text to build on
+              the card title and make up the bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary me-2">
+              Discuss
+            </a>
+            <button className="btn btn-primary" type="submit">
+              <i class="bi bi-hand-thumbs-up"></i>
+            </button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
