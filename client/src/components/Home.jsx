dff3174e37b85b50d76ebcaa386a2f1218bd74aa
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2022-06-18 12:40:13 +0500

    fix: navbar position

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -7,23 +7,7 @@ const Home = () => {
       <div className="row justify-content-center mt-5">
         <div className="card mt-4" style={{ width: '80%' }}>
           <div className="card-body">
-            <h5 className="card-title">zainsajid</h5>
-            <p className="card-text">
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content. Some quick example text to build on
-              the card title and make up the bulk of the card's content.
-            </p>
-            <a href="#" className="btn btn-primary me-2">
-              Discuss
-            </a>
-            <button className="btn btn-primary" type="submit">
-              <i class="bi bi-hand-thumbs-up"></i>
-            </button>
-          </div>
-        </div>
-        <div className="card mt-4" style={{ width: '80%' }}>
-          <div className="card-body">
-            <h5 className="card-title">zainsajid</h5>
+            <h5 className="card-title fw-bold">zainsajid</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content. Some quick example text to build on
@@ -35,12 +19,12 @@ const Home = () => {
             <button className="btn btn-primary me-2" type="submit">
               <i class="bi bi-hand-thumbs-up"></i>
             </button>
-            <span className="bold">+2</span>
+            <p className="d-inline fw-bold">+2</p>
           </div>
         </div>
         <div className="card mt-4" style={{ width: '80%' }}>
           <div className="card-body">
-            <h5 className="card-title">zainsajid</h5>
+            <h5 className="card-title fw-bold">zainsajid</h5>
             <p className="card-text">
               Some quick example text to build on the card title and make up the
               bulk of the card's content. Some quick example text to build on
@@ -49,9 +33,27 @@ const Home = () => {
             <a href="#" className="btn btn-primary me-2">
               Discuss
             </a>
-            <button className="btn btn-primary" type="submit">
+            <button className="btn btn-primary me-2" type="submit">
               <i class="bi bi-hand-thumbs-up"></i>
             </button>
+            <p className="d-inline fw-bold">+2</p>
+          </div>
+        </div>
+        <div className="card mt-4" style={{ width: '80%' }}>
+          <div className="card-body">
+            <h5 className="card-title fw-bold">zainsajid</h5>
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content. Some quick example text to build on
+              the card title and make up the bulk of the card's content.
+            </p>
+            <a href="#" className="btn btn-primary me-2">
+              Discuss
+            </a>
+            <button className="btn btn-primary me-2" type="submit">
+              <i class="bi bi-hand-thumbs-up"></i>
+            </button>
+            <p className="d-inline fw-bold">+2</p>
           </div>
         </div>
       </div>
