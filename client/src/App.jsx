25a7c3f474f533e66d42048aa88898832b461084
useracct <basilyusuf1709@gmail.com>
2022-06-18 07:50:51 -0400

    fix: updated authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,13 +6,13 @@ import { useAuth0 } from '@auth0/auth0-react'
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
-    const { user } = useAuth0();
+    const { isAuthenticated } = useAuth0();
     return (<div>
         <Routes>
             <Route index path="/" element={<Auth />} />
             <Route path="/home" element={<Home />} />
         </Routes>
-        {user !== undefined && <Navbar />}
+        {isAuthenticated && <Navbar />}
     </div>)
 }
 
