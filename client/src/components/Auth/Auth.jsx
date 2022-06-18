027c66d1bd4dffc82d5a28854f103fca1fc6ef71
useracct <basilyusuf1709@gmail.com>
2022-06-18 00:18:58 -0400

    feat: Adding auth0 API

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react'
 import home from '../../assets/home.gif'
 import deso from '../../assets/deSo.png'
 import auth0 from '../../assets/auth0.png'
@@ -6,6 +7,7 @@ import './Auth.css'
 
 
 const Auth = () => {
+    const { loginWithRedirect } = useAuth0();
     return (
         <div className="auth-container">
             <img className="auth-image" src={home}/>
@@ -14,7 +16,7 @@ const Auth = () => {
                     <img className="auth-button-image" src={deso}/>
                     <p>Jump in using DeSo</p>
                 </button>
-                <button className="auth-button-auth0">
+                <button onClick={() => loginWithRedirect()} className="auth-button-auth0">
                     <img className="auth-button-image" src={auth0}/>
                     <p>Hop in using Auth0</p>
                 </button>
--- client/src/index.js
+++ client/src/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './components/Home.jsx';
-import Navbar from './components/Navbar/Navbar.jsx';
-import Auth from './components/Auth/Auth.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from 'react-router-dom';
+import App from './App'
+
+const domain = process.env.REACT_APP_AUTH0_DOMAIN
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    {/* <Auth /> */}
-    <Routes>
-      <Route path="/home" element={<Home />} />
-    </Routes>
-    <Navbar />
-  </BrowserRouter>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    redirectUri='http://localhost:3000/home'
+  >
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+  </Auth0Provider>
 );
