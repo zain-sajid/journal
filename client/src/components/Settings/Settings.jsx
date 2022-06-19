a09ca19959586c222e496f1f7342b1001c7eb6a5
useracct <basilyusuf1709@gmail.com>
2022-06-19 04:03:03 -0400

    feat: log out using deso and auth0

--- client/src/components/Settings/Settings.jsx
+++ client/src/components/Settings/Settings.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import axios from 'axios';
+import DeSo from '../../assets/deSo.png'
+import auth0 from '../../assets/auth0.png'
+import Deso from "deso-protocol";
+import { useNavigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import '../Auth/Auth.css'
+const deso = new Deso();
 
 const Settings = () => {
   const [IoTChannel, setIoTChannel] = React.useState();
+  const navigate = useNavigate()
+  const { logout } = useAuth0();
   React.useEffect(() => {
     const fetchIoTChannel = async () => {
       try {
         const data = await axios.get(
-          'https://api.thingspeak.com/channels/1773828.json?api_key=0M1SSJLEI0UMM6XR'
-        );
+          'https://api.thingspeak.com/channels/1773828.json?api_key=0M1SSJLEI0UMM6XR',{'Access-Control-Allow-Origin': true,});
         setIoTChannel(data.data);
       } catch (err) {
         console.log(err);
@@ -55,6 +63,22 @@ const Settings = () => {
               </tbody>
             </table>
           </div>
+        </div>
+        <div className="row justify-content-center">
+          <button onClick={async () => {
+                const user = await deso.identity.logout(deso.identity.getUserKey() || '{}');
+                console.log(user)
+                navigate('/')
+            }}  className="auth-button-deso">
+            <img className="auth-button-image" src={DeSo} alt="deso-button"/>
+            <p>Log out using DeSo</p>
+          </button>
+          <button onClick={() => logout({
+            returnTo: 'http://localhost:3000/'
+          })} className="auth-button-auth0">
+              <img className="auth-button-image" src={auth0}  alt="auth0-button"/>
+              <p>Log out using Auth0</p>
+          </button>
         </div>
       </>
     )
